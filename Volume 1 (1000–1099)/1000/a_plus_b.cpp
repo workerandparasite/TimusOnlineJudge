33497f9ef155903234c73339c4b3ab98b4ab5b01
Miguel A. Muñoz <miguemunoz@openmailbox.org>
2016-10-25 22:13:36 +0100

    Simple solution for a (extremely) simple problem.

---
+++ Volume 1 (1000–1099)/1000/a_plus_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	cin >> a;
+	cin >> b;
+
+	cout << a+b << endl;
+
+	return 0;
+}
